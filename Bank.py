5b013db5ff06422a72afdadea58bf5311a63a008
Muhammad Azmi Nur Iman <113041333+langit13@users.noreply.github.com>
2022-09-13 23:40:12 +0700

    Add files via upload

---
+++ Bank.py
@@ -0,0 +1,14 @@
+
+sapaan=input("").lower()
+x=sapaan.split(',')
+
+if sapaan=="hello":
+    uang=0
+elif (x=="hello"):
+    uang=0
+elif sapaan[0]=="h":
+    uang=20
+else:
+    uang=100
+
+print('$', uang)
